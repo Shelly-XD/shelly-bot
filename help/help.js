2b1eb0a47d08b18534ca52fa8ce4f185e4e50409
Shelly <87390227+Shelly-XD@users.noreply.github.com>
2021-08-17 11:24:20 +0700

    Add files via upload

---
+++ help/help.js
@@ -0,0 +1,383 @@
+const help = (prefix) => {
+    return `╭──── ◪›「 𝒮𝒽𝑒𝓁𝓁𝓎 𝐵𝑜𝓉  」
+│
+│
+│
+╰────
+
+╭────
+│
+│#help
+│#donate
+│#sticker
+│#stickerwm
+│#broadcast
+│#hidetag
+│#tagstick
+│#clearall
+│
+╰────
+
+╭─「 Islami 」
+│#listsurah
+│#asmaulhusna
+│#alquran no_surah
+│#alquran no_surah/no_ayat
+│#alquranaudio no_surah
+│#alquranaudio no_surah/no_ayat
+│#kisahnabi nama_nabi
+│#jadwalsholat daerah
+│
+╰────
+
+╭─「 Downloader 」
+│#ytsearch query
+│#ytplay query
+│#ytplay2 query
+│#ytmp3 url_video
+│#ytmp32 url_video
+│#ytmp4 url_video
+│#ytmp42 url_video
+│#tiktoknowm url_video
+│#tiktokmusic url_video
+│#igdl url_post
+│#fbdl url_video
+│#jooxplay query
+│#spotify url_music
+│#spotifysearch query
+│#pinterest query
+│#pinterestdl url_pinterest
+│#pixiv query
+│#pixivdl url_pixiv
+│#zippyshare url_zippyshare
+│#telesticker url_pack
+│
+╰────
+
+╭─「 Movie & Story 」
+│#drakorongoing
+│#lk21 query
+│#wattpad url_wattpad
+│#wattpadsearch query
+│#cerpen
+│#ceritahoror
+│
+╰────
+
+╭─「 Searching 」
+│#shopee query
+│#google query
+│#gimage query
+│#gimage2 query
+│#konachan query
+│#playstore query
+│#stickerwa query
+│#wallpapersearch query
+│#wallpapersearch2 query
+│
+╰────
+
+╭─「 Random Text 」
+│#quotes
+│#quotesdilan
+│#quotesanime
+│#quotesimage
+│#faktaunik
+│#katabijak
+│#pantun
+│#bucin
+│#randomnama
+│
+╰────
+
+╭─「 AniManga 」
+│#wait
+│#manga query
+│#anime query
+│#character query
+│#kusonime url_kusonime
+│#kusonimesearch query
+│#otakudesu url_otakudesu
+│#otakudesusearch query
+│#nhentai kode_bom
+│#nhentaipdf kode_bom
+│#nhentaisearch query
+│#nekopoi url
+│#nekopoisearch query
+│
+╰────
+
+╭─「 Information 」
+│#jadwaltv channel
+│#jadwaltvnow
+│#jadwalbola
+│#qrreader
+│#heroml hero_name
+│#mlstalk id/server
+│#genshin character
+│#wikipedia query
+│#translate kode_negara text
+│#brainly query
+│#newsinfo
+│#cnnindonesia
+│#cnnnasional
+│#cnninternasional
+│#infogempa
+│#lirik query
+│#cuaca daerah
+│#kodepos query
+│#indbeasiswa
+│#hoax
+│#nsfwcheck
+│#ocr
+│
+╰────
+
+╭─「 Entertainment 」
+│#asupan
+│#wancak
+│#tebakgambar
+│#canceltebakgambar
+│#akinator
+│#cancelakinator
+│
+╰────
+
+╭─「 Creator 」
+│#ttp text
+│#ttp2 text
+│#ttp3 text
+│#ttp4 text
+│#attp text
+│#smoji emoji
+│#fakedonald text
+│#ktpmaker
+│
+╰────
+
+╭─「 Primbon 」
+│#artinama name
+│#jodoh name1 & name2 
+│#weton tanggal bulan tahun
+│#jadian tanggal bulan tahun
+│#tebakumur name
+│
+╰────
+
+╭─「 Other 」
+│#ssweb link
+│#ssweb2 link
+│#shortlink link
+│#spamsms nomor
+│
+╰────
+
+╭─「 Text Pro Me 」
+│#blackpink text
+│#neon text
+│#greenneon text
+│#advanceglow text
+│#futureneon text
+│#sandwriting text
+│#sandsummer text
+│#sandengraved text
+│#metaldark text
+│#neonlight text
+│#holographic text
+│#text1917 text
+│#minion text
+│#deluxesilver text
+│#newyearcard text
+│#bloodfrosted text
+│#halloween text
+│#jokerlogo text
+│#fireworksparkle text
+│#natureleaves text
+│#bokeh text
+│#toxic text
+│#strawberry text
+│#box3d text
+│#roadwarning text
+│#breakwall text
+│#icecold text
+│#luxury text
+│#cloud text
+│#summersand text
+│#horrorblood text
+│#thunder text
+│#pornhub text1 text2
+│#glitch text1 text2
+│#avenger text1 text2
+│#space text1 text2
+│#ninjalogo text1 text2
+│#marvelstudio text1 text2
+│#lionlogo text1 text2
+│#wolflogo text1 text2
+│#steel3d text1 text2
+│#wallgravity text1 text2
+│
+╰────
+
+╭─「 Photo Oxy 」
+│#shadow text
+│#cup text
+│#cup1 text
+│#romance text
+│#smoke text
+│#burnpaper text
+│#lovemessage text
+│#undergrass text
+│#love text
+│#coffe text
+│#woodheart text
+│#woodenboard text
+│#summer3d text
+│#wolfmetal text
+│#nature3d text
+│#underwater text
+│#golderrose text
+│#summernature text
+│#letterleaves text
+│#glowingneon text
+│#fallleaves text
+│#flamming text
+│#harrypotter text
+│#carvedwood text
+│#tiktok text1 text2
+│#arcade8bit text1 text2
+│#battlefield4 text1 text2
+│#pubg text1 text2
+│
+╰────
+
+╭─「 Ephoto 360 」
+│#wetglass text
+│#multicolor3d text
+│#watercolor text
+│#luxurygold text
+│#galaxywallpaper text
+│#lighttext text
+│#beautifulflower text
+│#puppycute text
+│#royaltext text
+│#heartshaped text
+│#birthdaycake text
+│#galaxystyle text
+│#hologram3d text
+│#greenneon text
+│#galaxybat text
+│#snow3d text
+│#birthdayday text
+│#goldplaybutton text
+│#silverplaybutton text
+│#freefire text
+│
+╰────
+
+╭─「 Random Image  」
+│#art
+│#bts
+│#exo
+│#elf
+│#loli
+│#neko
+│#waifu
+│#shota
+│#husbu
+│#sagiri
+│#shinobu
+│#megumin
+│#wallnime
+│#chiisaihentai
+│#trap
+│#blowjob
+│#yaoi
+│#ecchi
+│#hentai
+│#ahegao
+│#sideoppai
+│#animefeets
+│#animebooty
+│#animethighss
+│#hentaiparadise
+│#animearmpits
+│#hentaifemdom
+│#hentai4everyone
+│#yuri
+│#trap
+│#anal
+│#holo
+│#waifu
+│#pussy
+│#femdom
+│#cuddle
+│#hentai
+│#eroyuri
+│
+╰◪›「 𝒮𝒽𝑒𝓁𝓁𝓎 𝐵𝑜𝓉  」 `
+}
+exports.help = help
+
+const donate = (sender) => {
+    return `ANTI DONASI DEK`
+}
+exports.donate = donate
+
+
+const bahasa = () => {
+    return `
+List Bahasa :
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
